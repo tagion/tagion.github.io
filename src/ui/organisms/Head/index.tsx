5125f79c92edf8475fb664f4d92f5f8b81fef56f
tini2n <tini2n@gmail.com>
2022-02-14 13:07:00 +0200

    Updating meta description, keywords, title

--- src/ui/organisms/Head/index.tsx
+++ src/ui/organisms/Head/index.tsx
@@ -3,17 +3,16 @@ import NextHead from 'next/head';
 const Head = () => {
 	return (
 		<NextHead>
-			<title>Tagion. We are New Money</title>
+			<title>Tagion: the independent money system</title>
 			<link rel='shortcut icon' type='image/svg' href='/favicon.svg' />
 			<meta charSet='UTF-8'></meta>
 			<meta
 				name='description'
-				content='Tagion is a decentralized non-political monetary system, used and governed openly as a common
-						good.'
+				content='Welcome to the future of money with Tagion - the monetary system, allowing you to participate in the global economy directly, immediately, and without special permission. This money system is decentralized, fast, non-political, and stable. Join Tagion to trade goods, services, and digital assets without the limitation of banks and credit card companies.'
 			></meta>
 			<meta
 				name='keywords'
-				content='crypto, money, tagion, decentralized, network, digital, smart, social'
+				content='tagion, tagions, money, monetary system, decentralized, independent, community, economy, global, worldwide, non-political, common goods, fast, future, openly governed'
 			></meta>
 			<meta name='author' content='Vlady aka Hustler Engineer'></meta>
 			<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'></meta>
